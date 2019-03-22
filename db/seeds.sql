2bddf1a9461460943cf2d5c906e58f34e6c08d4c
PENA5 <44989115+PENA5@users.noreply.github.com>
2019-03-21 23:20:35 -0500

    views/

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Bacon Chesse Burger');
+INSERT INTO burgers (name) VALUES ('Grilled Chicken Sandwich');
+INSERT INTO burgers (name) VALUES ('Regular Chesse Burger');
